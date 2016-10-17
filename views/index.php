2800cc0861a677e829b601a3a74972563936071e
ankitbasarkar <ankit.basarkar@gmail.com>
2016-10-17 15:39:41 -0700

    First COmmit

---
+++ views/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ankit
+ * Date: 10/17/2016
+ * Time: 3:25 PM
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Five Thousand Characters</title>
+</head>
+<body>
+<h1>Five Thousand Characters</h1>
+<a src=""></a>
+
+</body>
+</html>
