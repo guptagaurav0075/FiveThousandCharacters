4be0b2397e51f5474cef97e37e55f99e1c05287e
bhargavat <bhargava.ramisetty@gmail.com>
2016-10-21 19:23:18 -0700

    Views with HTML code

---
+++ src/views/storyView.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Five Thousand Characters - Story Title</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1 style="text-align: center">
+		<a href="LandingView.php">Five Thousand Characters</a>
+	</h1>
+	<div>
+		<p>Your rating: 1 2 3 4 5</p>
+		<p>Average Rating: </p>
+	</div>
+</body>
+</html>
--- src/views/writeSomething.php
+++ src/views/writeSomething.php
@@ -1,8 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ankit
- * Date: 10/17/2016
- * Time: 3:50 PM
- */
-echo "Hello";
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Five Thousand Characters - Write Something</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<form name="myform" method="get">
+		<h1 style="text-align: center">Five Thousand Characters - Write Something</h1>
+		<label for="title">Title</label> <br>
+			<input type="text" id ="title"></input> <br>
+		<label for="author">Author</label> <br>
+			<input type="text" id ="author"></input> <br>
+		<label for="identifier">Identifier</label> <br>
+			<input type="text" id ="identifier"></input> <br> <br>
+		<textarea rows="5" cols="40"></textarea> <br>
+
+		<button>Reset</button> <button>Save</button>
+	</form>
+</body>
+</html>
